2fda4ee26ef7f2a05aa8ae5334e7d8433c40e6f2
Timothy Appnel <tim@appnel.com>
2011-01-04 15:54:15 -0500

    Various fixes and tidying based discovered from the bylines plugin development.

--- plugins/pullquotes/lib/Melody/Pullquotes.pm
+++ plugins/pullquotes/lib/Melody/Pullquotes.pm
@@ -8,22 +8,34 @@ sub add_pull_quote_field {
     my ( $eh, $app, $param, $tmpl ) = @_;
     return unless $tmpl->isa('MT::Template');
     my $q = $app->can('query') ? $app->query : $app->param;
-    my $model = $app->mode('view_page') ? 'page' : 'entry';  # will this work?
+
+    # my $model = $app->mode('view_page') ? 'page' : 'entry';  # will this work?
+    my $model      = 'entry';
     my $pull_quote = '';
     if ( my $id = $q->param('id') ) {
         require MT::Util;
         my $obj = $app->model($model)->load( $id, { cached_ok => 1 } );
         $pull_quote = MT::Util::encode_html( $obj->pull_quote )
-          if $obj && $obj->pull_quote;
+          if $obj && $obj->pull_quote;    # Presume value will not be 0.
     }
-    my $innerHTML
-      = qq{ <textarea name="pull_quote" id="pull_quote" class="full-width short" cols="" rows="" mt:watch-change="1">$pull_quote</textarea> };
+    my $innerHTML = qq{ 
+		<div class="textarea-wrapper">
+			<input name="pull_quote" id="pull_quote" class="full-width" mt:watch-change="1" value="$byline"/>
+		</div> 
+      };
     my $host_node 
-      = $tmpl->getElementById('tags-field')
-      or $tmpl->getElementById('text-field')
+      = $tmpl->getElementById('tags')
+      or $tmpl->getElementById('text')
       or return $app->error('getElementById failed');
-    my $block_node = $tmpl->createElement( 'app:setting',
-                              { id => 'pull_quote', label => 'pull_quote' } );
+    my $block_node =
+      $tmpl->createElement(
+                            'app:setting',
+                            {
+                               id          => 'pull_quote',
+                               label       => 'pull_quote',
+                               label_class => 'top-label'
+                            }
+      );
     $block_node->innerHTML($innerHTML);
     return $tmpl->insertBefore( $block_node, $host_node )
       or $app->error('failed to insertBefore');
